004c6a9b2a8b163491dccac1aff9829b1d52fa61
akmaccuish <89708503+akmaccuish@users.noreply.github.com>
2021-12-07 17:33:40 -0500

    Update final project variable code.py

--- final project variable code.py
+++ final project variable code.py
@@ -9,20 +9,31 @@ import gurobipy as gp
 from gurobipy import GRB
 
 year = 10
-
+warehouse = 4
+# Indexed sets
 stores = range(len(demandfy)) # k
-plants = range(len(concostfy)) # i
-warehouses = range(4) # j
+plants = range(len(concostfy)) # i 
+warehouses = range(warehouse) # j
 years = range(year) # t
 
 
-r = gp.Model("Retail Store")
+r = gp.Model("Model")
 
+# Variables
 zvars = r.addVars(plants, years, vtype=GRB.CONTINUOUS, obj = planttowh, name = "z")
 xvars = r.addVars(plants, warehouses, years, vtype=GRB.CONTINUOUS, obj = planttowh, name = "x")
 yvars = r.addVars(warehouses, stores, years, vtype=GRB.CONTINUOUS, obj=whtocust, name = "y")
 ivars = r.addVars(warehouses, years, vtype=GRB.CONTINUOUS, obj=whtocust, name = "i")
+lamda1vars = r.addVars(plants, years, vtype=GRB.CONTINUOUS, obj = planttowh, name = "lamda1")
+lamda2vars = r.addVars(plants, years, vtype=GRB.CONTINUOUS, obj = planttowh, name = "lamda2")
+lamda3vars = r.addVars(plants, years, vtype=GRB.CONTINUOUS, obj = planttowh, name = "lamda3")
 pvars = r.addVars(plants, years, vtype=GRB.CONTINUOUS, obj = fixedcostplant, lb = 0, ub = 1,  name = "pvar")
+e1vars = r.addVars(plants, years, vtype=GRB.CONTINUOUS, obj = fixedcostplant, lb = 0, ub = 1,  name = "e1")
+e2vars = r.addVars(plants, years, vtype=GRB.CONTINUOUS, obj = fixedcostplant, lb = 0, ub = 1,  name = "e2")
 fvars = r.addVars(plants, years, vtype=GRB.CONTINUOUS, obj = fixedcostplant, lb = 0, ub = 1,  name = "fvar")
 gvars = r.addVars(plants, years, vtype=GRB.CONTINUOUS, obj = fixedcostwh, lb = 0, ub = 1,  name = "gvar")
 hvars = r.addVars(plants, years, vtype=GRB.CONTINUOUS, obj = fixedcostwh, lb = 0, ub = 1,  name = "hvar")
+
+# Constraints
+m.addConstrs((gvars[i][t] == 1 - pvars[i][t-1] for i in plants for t in years), "Reopening cost")
+m.addConstrs((fvars[i][t] == 1 - pvars[i][t+1] for i in plants for t in years), "Shut down cost")
